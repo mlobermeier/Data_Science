830054372ed64b239ec19815be2092b6ebbb7210
Maja Lobermeier <maja-lobermeier@t-online.de>
2025-06-12 10:24:10 +0200

    python file change

--- mergesort.py
+++ mergesort.py
@@ -1,8 +1,11 @@
-def ASSIGNMENT(new_list, i, old_list, j):
+import matplotlib.pyplot as plt
+
+
+def assignment(new_list, i, old_list, j):
     new_list[i] = old_list[j]
 
 
-def mergeSort(list_to_sort_by_merge):
+def merge_sort(list_to_sort_by_merge):
     if (
         len(list_to_sort_by_merge) > 1
         and not len(list_to_sort_by_merge) < 1
@@ -12,25 +15,27 @@ def mergeSort(list_to_sort_by_merge):
         left = list_to_sort_by_merge[:mid]
         right = list_to_sort_by_merge[mid:]
 
-        mergeSort(left)
-        mergeSort(right)
+        merge_sort(left)
+        merge_sort(right)
 
-        l = 0
+        length_l = 0
         r = 0
         i = 0
 
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
+        while length_l < len(left) and r < len(right):
+            if left[length_l] <= right[r]:
+                assignment(new_list=list_to_sort_by_merge,
+                           i=i, old_list=left, j=length_l)
+                length_l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                assignment(new_list=list_to_sort_by_merge, i=i, old_list=right,
+                           j=r)
                 r += 1
             i += 1
 
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
+        while length_l < len(left):
+            list_to_sort_by_merge[i] = left[length_l]
+            length_l += 1
             i += 1
 
         while r < len(right):
@@ -39,13 +44,11 @@ def mergeSort(list_to_sort_by_merge):
             i += 1
 
 
-import matplotlib.pyplot as plt
-
 my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
-mergeSort(my_list)
+merge_sort(my_list)
 x = range(len(my_list))
 plt.plot(x, my_list)
 plt.show()
